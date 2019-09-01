3da2cf73e748cb9e83c4ad63b5b3b13267cf77a9
fame <famedirtyshit@gmail.com>
2019-09-01 19:06:38 +0700

    [add] function

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ const config = {
         default: 'arcade',
         arcade: {
             debug: false,
-            gravity: {y:300}
+            gravity: {y:0}
         }
     },
     scene: [
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,11 +1,9 @@
-let width
-let height
-let x
-let y
-let cursor
+let bg1
 let player
-let checkpoint
-let platforms
+let mon
+let cursor
+let ammo
+
 class GameScene extends Phaser.Scene {
     constructor(test) {
         super({
@@ -14,81 +12,83 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('bg','src/image/bg.jpg')
-        this.load.spritesheet('ch','src/image/character.png',{frameWidth:416,frameHeight:454})
-        this.load.image('ground','src/image/ground.png')
-        this.load.image('path','src/image/long path.png')
-        this.load.spritesheet('check','src/image/checkpoint.png',{frameWidth:164,frameHeight:414})
+        this.load.image('bg', 'src/image/bg.jpg')
+        //this.load.image('bg', 'src/image/backgound.png')
+        this.load.image('planet', 'src/image/bigplanet.png')
+        this.load.image('far', 'src/image/farplanet.png')
+        this.load.image('ring', 'src/image/ringplanet.png')
+        this.load.image('star', 'src/image/spacestar.png')
+        this.load.spritesheet('ch', 'src/image/character.png', { frameWidth: 416, frameHeight: 454 })
     }
 
     create() {
 
-        
-        console.log(this)
-        width = this.scene.scene.physics.world.bounds.width
-        height = this.scene.scene.physics.world.bounds.height
-        x = width*0.5
-        y = height*0.5
-        this.add.image(x,y,'bg')
-        
-        platforms = this.physics.add.staticGroup()
-        platforms.create(x,y*2,'ground')
-        platforms.create(300,300,'path')
-        checkpoint = this.physics.add.sprite(400,330,'check').setScale(0.2).setImmovable()
-        player =  this.physics.add.sprite(100,475,'ch').setScale(0.3)
-        player.setCollideWorldBounds(true)
-        checkpoint.setCollideWorldBounds(true)
-        this.physics.add.collider(player,platforms)
-        this.physics.add.collider(player,checkpoint)
-        cursor = this.input.keyboard.createCursorKeys()
-
+        bg1 = this.add.tileSprite(0, 0, 800, 600, 'bg').setOrigin(0, 0)
+        player = this.physics.add.sprite(200,550, 'ch').setScale(0.2).setCollideWorldBounds(true)
+        mon = this.physics.add.image(400,-50,'ring')
         this.anims.create({
             key:'right',
             frames:this.anims.generateFrameNumbers('ch',{start:0,end:3}),
-            frameRate:10,
+            framerate:10,
             repeat:-1
-
-        })
-
+        }
+        )
         this.anims.create({
             key:'stop',
-            frames:this.anims.generateFrameNumbers('ch',{start:0,end:0}),
-            frameRate:10,
-            repeat:-1
-
+            frames:[{key:'ch',frame:0}],
+            framerate:10,
+            repeate:-1
         })
 
-        this.anims.create({
-            key:'check',
-            frames:this.anims.generateFrameNumbers('check',{start:0,end:3}),
-            frameRate:10,
-            repeat:-1
-        })
+        
 
+        cursor = this.input.keyboard.createCursorKeys()
 
-
-        checkpoint.anims.play('check')
+        this.time.addEvent({
+            delay: 1000,
+            callback:function(){
+            ammo = this.createAmmo();
+            //ammo.setVelocityY(-200)
+           /* if(ammo.y<=0){
+                ammo.destroy
+            }*/
+        },     
+            callbackScope: this,
+            loop: true
+          });
 
     }
 
     update() {
-        if(cursor.left.isDown){
-            player.setVelocityX(-330)
-        }else if(cursor.right.isDown){
-            player.setVelocityX(330)
-            player.anims.play('right',true)
-        }else if(cursor.up.isDown&&player.body.touching.down){
-            player.setVelocityY(-500)
-        }else if(cursor.down.isDown){
-            player.setVelocityY(330)
+     
+        mon.setVelocityY(100)
+        bg1.tilePositionY -= 1
+         if(mon.y>=600){
+             mon.destroy
+             mon = this.physics.add.image(400,-50,'ring')
+         }
+         if(cursor.left.isDown){
+             player.setVelocityX(-300)
+         }else if(cursor.right.isDown){
+             player.anims.play('right',true)
+            player.setVelocityX(300)
         }else{
             player.setVelocityX(0)
-            player.setVelocityY(0)
-            player.anims.play('stop')
+            player.anims.play('stop',true)
         }
-            
+
+
+
+
+    }
+
+    createAmmo(){
+        this.physics.add.image(player.x,player.y-60,'planet').setScale(0.3)
         
     }
+
+
+
 }
 
 export default GameScene;
